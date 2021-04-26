dff9d192de8db507d46930e14e523c53e2a84db7
andreiamartinho82 <77107392+andreiamartinho82@users.noreply.github.com>
2021-04-26 15:49:17 +0100

    search engine form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-     
-    </div>
+      <div className="container">
+        <header>
+          <img src={logo} className="logo" alt="logo"/>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="footer">Coded by Andreia Martinho</footer>
+
+      </div>
+     </div>
   );
 }
 
