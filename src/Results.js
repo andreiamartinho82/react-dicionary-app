971bb17926cd4e5a89cea98219aeecb22ec0cc49
andreiamartinho82 <77107392+andreiamartinho82@users.noreply.github.com>
2021-04-27 18:33:50 +0100

    added Phonetics component
    added Phonetics component
    link to audio file

--- src/Results.js
+++ src/Results.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Meaning from "./Meaning";
-
+import Phonetic from "./Phonetic";
 export default function(props){
 
-    console.log(props.results);
-if (props.results){
+   if (props.results){
     return (
         <div className="Results">
             <h2>
                 {props.results.word}
             </h2>
+            {props.results.phonetics.map(function(phonetic, index){
+                return(
+                    <div key={index}>
+                        <Phonetic phonetic={phonetic} />
+                    </div>
+                );
+
+            })}
 
             {props.results.meanings.map(function (meaning, index){
                 return (
